f64cba216a99d9c273a8e379d037c92fb7e91628
Ala Ben Yahia <alabny18@gmail.com>
2021-01-30 19:19:58 +0100

    Add theme

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import {createMuiTheme} from "@material-ui/core";
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#673ab7',
+            light: '#9a67ea',
+            dark: '#320b86'
+        },
+        secondary: {
+            main: '#ff4081',
+            light: '#ff79b0',
+            dark: '#c60055'
+        }
+    }
+});
