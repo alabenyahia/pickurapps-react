ee695cd01d55180304aaff635ba2f0e3e2c8e74b
Ala Ben Yahia <alabny18@gmail.com>
2021-02-01 00:10:02 +0100

    Add section data

--- src/baccalc/sectionData.js
+++ src/baccalc/sectionData.js
@@ -2,37 +2,503 @@ const sectionData = {
     math: {
         sectionName: 'Mathématiques',
         iconName: 'calculate',
-        to: '/baccalc/math'
+        to: '/baccalc/math',
+        matiere: {
+            sesPrin: {
+                math: {
+                    name: 'Mathématiques',
+                    coef: 4
+                },
+                phys: {
+                    name: 'Physiques',
+                    coef: 4
+                },
+                sci: {
+                    name: 'Sciences',
+                    coef: 1
+                },
+                philo: {
+                    name: 'Philosophie',
+                    coef: 1
+                },
+                fr: {
+                    name: 'Français',
+                    coef: 1
+                },
+                ang: {
+                    name: 'Anglais',
+                    coef: 1
+                },
+                ar: {
+                    name: 'Arabe',
+                    coef: 1
+                },
+                info: {
+                    name: 'Informatique',
+                    coef: 1
+                },
+                spr: {
+                    name: 'Sport',
+                    coef: 1
+                },
+                opt: {
+                    name: 'Option',
+                    coef: 1
+                }
+            },
+            sesCont: {
+                mathCtrl: {
+                    name: 'Mathématiques',
+                },
+                physCtrl: {
+                    name: 'Physiques',
+                },
+                sciCtrl: {
+                    name: 'Sciences',
+                },
+                frCtrl: {
+                    name: 'Français',
+                },
+                angCtrl: {
+                    name: 'Anglais',
+                },
+                arCtrl: {
+                    name: 'Arabe',
+                }
+            }
+        }
     },
     sci: {
         sectionName: 'Sciences',
         iconName: 'science',
-        to: '/baccalc/sci'
+        to: '/baccalc/sci',
+        matiere: {
+            sesPrin: {
+                math: {
+                    name: 'Mathématiques',
+                    coef: 3
+                },
+                phys: {
+                    name: 'Physiques',
+                    coef: 4
+                },
+                sci: {
+                    name: 'Sciences',
+                    coef: 4
+                },
+                philo: {
+                    name: 'Philosophie',
+                    coef: 1
+                },
+                fr: {
+                    name: 'Français',
+                    coef: 1
+                },
+                ang: {
+                    name: 'Anglais',
+                    coef: 1
+                },
+                ar: {
+                    name: 'Arabe',
+                    coef: 1
+                },
+                info: {
+                    name: 'Informatique',
+                    coef: 1
+                },
+                spr: {
+                    name: 'Sport',
+                    coef: 1
+                },
+                opt: {
+                    name: 'Option',
+                    coef: 1
+                }
+            },
+            sesCont: {
+                mathCtrl: {
+                    name: 'Mathématiques',
+                },
+                physCtrl: {
+                    name: 'Physiques',
+                },
+                sciCtrl: {
+                    name: 'Sciences',
+                },
+                frCtrl: {
+                    name: 'Français',
+                },
+                angCtrl: {
+                    name: 'Anglais',
+                },
+                arCtrl: {
+                    name: 'Arabe',
+                }
+            }
+        }
     },
     eco: {
         sectionName: 'Économie',
         iconName: 'account_balance',
-        to: '/baccalc/eco'
+        to: '/baccalc/eco',
+        matiere: {
+            sesPrin: {
+                math: {
+                    name: 'Mathématiques',
+                    coef: 2
+                },
+                eco: {
+                    name: 'Économie',
+                    coef: 3
+                },
+                gest: {
+                    name: 'Gestion',
+                    coef: 3
+                },
+                hg: {
+                    name: 'Histoire & Geo',
+                    coef: 2
+                },
+                philo: {
+                    name: 'philosophie',
+                    coef: 1
+                },
+                fr: {
+                    name: 'Français',
+                    coef: 1
+                },
+                ang: {
+                    name: 'Anglais',
+                    coef: 1
+                },
+                ar: {
+                    name: 'Arabe',
+                    coef: 1
+                },
+                info: {
+                    name: 'Informatique',
+                    coef: 1
+                },
+                spr: {
+                    name: 'Sport',
+                    coef: 1
+                },
+                opt: {
+                    name: 'Option',
+                    coef: 1
+                }
+            },
+            sesCont: {
+                mathCtrl: {
+                    name: 'Mathématiques',
+                },
+                ecoCtrl: {
+                    name: 'Économie',
+                },
+                gestCtrl: {
+                    name: 'Gestion',
+                },
+                hgCtrl: {
+                    name: 'Histoire & Geo',
+                },
+                frCtrl: {
+                    name: 'Français',
+                },
+                angCtrl: {
+                    name: 'Anglais',
+                },
+                arCtrl: {
+                    name: 'Arabe',
+                }
+            }
+        }
     },
     tech: {
         sectionName: 'Technique',
         iconName: 'build',
-        to: '/baccalc/tech'
+        to: '/baccalc/tech',
+        matiere: {
+            sesPrin: {
+                math:{
+                    name: 'Mathématiques',
+                    coef: 3
+                },
+                phys:{
+                    name: 'Physiques',
+                    coef: 3
+                },
+                tech:{
+                    name: 'Technique',
+                    coef: 4
+                },
+                philo:{
+                    name: 'Philosophie',
+                    coef: 1
+                },
+                fr:{
+                    name: 'Français',
+                    coef: 1
+                },
+                ang:{
+                    name: 'Anglais',
+                    coef: 1
+                },
+                ar:{
+                    name: 'Arabe',
+                    coef: 1
+                },
+                info:{
+                    name: 'Informatique',
+                    coef: 1
+                },
+                spr:{
+                    name: 'Sport',
+                    coef: 1
+                },
+                opt:{
+                    name: 'Option',
+                    coef: 1
+                },
+            },
+            sesCont: {
+                mathCtrl: {
+                    name: 'Mathématiques',
+                },
+                techCtrl: {
+                    name: 'Technique',
+                },
+                physCtrl: {
+                    name: 'Physiques',
+                },
+                frCtrl: {
+                    name: 'Français',
+                },
+                angCtrl: {
+                    name: 'Anglais',
+                },
+                arCtrl: {
+                    name: 'Arabe',
+                }
+            }
+        }
     },
     letr: {
         sectionName: 'Lettres',
         iconName: 'menu_book',
-        to: '/baccalc/letr'
+        to: '/baccalc/letr',
+        matiere: {
+            sesPrin: {
+                pns:{
+                    name: 'Pensée Islamique',
+                    coef: 1
+                },
+                hg:{
+                    name: 'Histoire & Geo',
+                    coef: 3
+                },
+                philo:{
+                    name: 'Philosophie',
+                    coef: 4
+                },
+                fr:{
+                    name: 'Français',
+                    coef: 2
+                },
+                ang:{
+                    name: 'Anglais',
+                    coef: 2
+                },
+                ar:{
+                    name: 'Arabe',
+                    coef: 4
+                },
+                info:{
+                    name: 'Informatique',
+                    coef: 1
+                },
+                spr:{
+                    name: 'Sport',
+                    coef: 1
+                },
+                opt:{
+                    name: 'Option',
+                    coef: 1
+                }
+            },
+            sesCont: {
+                philoCtrl: {
+                    name: 'Philosophie',
+                },
+                hgCtrl: {
+                    name: 'Histoire & Geo',
+                },
+                frCtrl: {
+                    name: 'Français',
+                },
+                angCtrl: {
+                    name: 'Anglais',
+                },
+                arCtrl: {
+                    name: 'Arabe',
+                }
+            }
+        }
     },
     info: {
         sectionName: 'Informatique',
         iconName: 'laptop',
-        to: '/baccalc/info'
+        to: '/baccalc/info',
+        matiere: {
+            sesPrin: {
+                math: {
+                    name: 'Mathématiques',
+                    coef: 3
+                },
+                phys: {
+                    name: 'Physiques',
+                    coef: 2
+                },
+                algo: {
+                    name: 'Algorithmes',
+                    coef: 3
+                },
+                bd: {
+                    name: 'BD',
+                    coef: 1.5
+                },
+                tic: {
+                    name: 'TIC',
+                    coef: 1.5
+                },
+                philo: {
+                    name: 'Philosophie',
+                    coef: 1
+                },
+                fr: {
+                    name: 'Français',
+                    coef: 1
+                },
+                ang: {
+                    name: 'Anglais',
+                    coef: 1
+                },
+                ar: {
+                    name: 'Arabe',
+                    coef: 1
+                },
+                spr: {
+                    name: 'Sport',
+                    coef: 1
+                },
+                opt: {
+                    name: 'Option',
+                    coef: 1
+                },
+            },
+            sesCont: {
+                mathCtrl: {
+                    name: 'Mathématiques',
+                },
+                algoCtrl: {
+                    name: 'Algorithmes',
+                },
+                physCtrl: {
+                    name: 'Physiques',
+                },
+                bdCtrl: {
+                    name: 'BD',
+                },
+                frCtrl: {
+                    name: 'Français',
+                },
+                angCtrl: {
+                    name: 'Anglais',
+                },
+                arCtrl: {
+                    name: 'Arabe',
+                }
+            }
+        }
     },
     spr: {
         sectionName: 'Sport',
         iconName: 'sports_soccer',
-        to: '/baccalc/spr'
+        to: '/baccalc/spr',
+        matiere: {
+            sesPrin: {
+                math: {
+                    name: 'Mathématiques',
+                    coef: 1
+                },
+                phys: {
+                    name: 'Physiques',
+                    coef: 1
+                },
+                sci: {
+                    name: 'Sciences',
+                    coef: 3
+                },
+                sprtheo: {
+                    name: 'Sport Théorique',
+                    coef: 0.5
+                },
+                sprpra: {
+                    name: 'Sport Pratique',
+                    coef: 2.5
+                },
+                eduphys: {
+                    name: 'Education Physique',
+                    coef: 1
+                },
+                philo: {
+                    name: 'Philosophie',
+                    coef: 1.5
+                },
+                fr: {
+                    name: 'Français',
+                    coef: 1.5
+                },
+                ang: {
+                    name: 'Anglais',
+                    coef: 1.5
+                },
+                ar: {
+                    name: 'Arabe',
+                    coef: 1
+                },
+                info: {
+                    name: 'Informatique',
+                    coef: 0.5
+                },
+                opt: {
+                    name: 'Option',
+                    coef: 1
+                }
+            },
+            sesCont: {
+                mathCtrl: {
+                    name: 'Mathématiques',
+                },
+                sciCtrl: {
+                    name: 'Sciences',
+                },
+                physCtrl: {
+                    name: 'Physiques',
+                },
+                sprtheoCtrl: {
+                    name: 'Sport Theorique',
+                },
+                frCtrl: {
+                    name: 'Français',
+                },
+                angCtrl: {
+                    name: 'Anglais',
+                },
+                arCtrl: {
+                    name: 'Arabe',
+                }
+            }
+        }
     }
 
 };
