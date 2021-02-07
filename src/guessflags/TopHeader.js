543d245ccf8c9949b02ca8e9cb44d461262d887f
Ala Ben Yahia <alabny18@gmail.com>
2021-02-08 00:20:21 +0100

    Add guessflags game data & game topheader back link

--- src/guessflags/TopHeader.js
+++ src/guessflags/TopHeader.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {IconButton} from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import coloredCoins from "./imgs/coin-colored.svg";
+import {NavLink} from "react-router-dom";
 const StyledHeader = styled.div`
   background-color: #7D5A5A;
   padding: 16px;
@@ -15,9 +16,12 @@ function TopHeader() {
 
     return (
         <StyledHeader>
-            <IconButton size="small" aria-label="back" color="inherit">
-                <ArrowBackIcon />
-            </IconButton>
+            <NavLink to='/guessflags' exact style={{color: 'white'}}>
+                <IconButton size="small" aria-label="back" color="inherit">
+                    <ArrowBackIcon />
+                </IconButton>
+            </NavLink>
+
             <p style={{margin:0, pointerEvents: 'none'}}>1/10</p>
             <div style={{display: 'flex', alignItems: 'center', pointerEvents: 'none'}}>
                 <span style={{marginRight: '8px'}}>2000</span><img width={28} height={28} src={coloredCoins} alt="colored-coins"/>
